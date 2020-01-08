f69af7ed0981e02a22e35c9fa897f110ead809e5
Alice Brunel <alc.brunel@gmail.com>
2020-01-08 17:31:55 +0000

    Added labels to the user new form, and built a create action in the User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
 
-	def index
-	end
+  def index
+  end
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-
+  def create
+    if (user = User.create user_params)
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      render 'new'
+    end
+  end
 end
