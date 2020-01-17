72c020a367483514e0fb74b891bb5b5c6bbe0c23
Alice Brunel <alc.brunel@gmail.com>
2020-01-17 11:04:14 +0000

    Rollback last migration and deleted the AddTypeToUsers, because realised I need to create a model for user type to allow multiple checkboxes selection. Created Usertype model.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def show 
+	@user = User.find_by(id:params[:id])
+  end
+
+
    private
 
   def user_params
-    params.require(:user).permit(:username, :email, :type, :password_digest)
+    params.require(:user).permit(
+	:username,
+	:email,
+	:type,
+	:password_digest
+	)
   end
 
 end
