800ee226e5c381f6860d0a6a5c7037ebd9af93b8
Alice Brunel <alc.brunel@gmail.com>
2020-01-18 18:59:36 +0000

    Completed the Artworks controller with index, show, create and specified the artworks parameters

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,2 +1,29 @@
 class ArtworksController < ApplicationController
+
+	def index
+		@artworks = Artwork.all
+	end
+
+	def show 
+		@artwork = Artwork.find_by(id: params[:id])
+	end
+
+	def create 
+		@artwork = Artwork.find_by(id: params[:id])
+	end 
+
+
+	def	private 
+
+	def artwork_params 
+		params.require(:artwork).permit(
+		:name
+		:description
+		:artist_id
+		:picture
+		:price
+		:technic
+		)
+	
+	end
 end
